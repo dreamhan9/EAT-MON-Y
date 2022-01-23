bc5b066e7fca60d0a843bd46c15ea9bf8db1347b
dreamhan9 <98266592+dreamhan9@users.noreply.github.com>
2022-01-23 21:47:00 +0800

    Update rank.php

--- rank.php
+++ rank.php
@@ -2,8 +2,8 @@
 <html>
 
 <head>
-  <title>吃掉小鹿乃-排行榜</title>
-  <meta item="description" content="来看神仙" />
+  <title>吃掉mon南山-排行榜</title>
+  <meta item="description" content="来看数学学霸" />
   <meta itemprop="image" content="https://www.thac.cc/kano/res/logo.jpg" />
   <meta charset="utf-8" />
   <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1.0" />
@@ -45,7 +45,7 @@
     <a href="?lbtype=day<?php echo $CurrentUser ? "&name=" . $CurrentUser : "" ?>"><button type="button" class="btn btn-outline-secondary btn-sm">日榜</button></a>
     <a href="?lbtype=week<?php echo $CurrentUser ? "&name=" . $CurrentUser : "" ?>"><button type="button" class="btn btn-outline-secondary btn-sm">周榜</button></a>
     <a href="?lbtype=month<?php echo $CurrentUser ? "&name=" . $CurrentUser : "" ?>"><button type="button" class="btn btn-outline-secondary btn-sm">月榜</button></a>
-    <br/>tips:大家不要作弊哦~ฅ'ω'ฅ♪
+    <br/>tips:大家不要卷哦~ฅ'ω'ฅ♪
   </div>
   <div class="list-group">
     <?php
